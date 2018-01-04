c15f73aa6dcec2674b40c2d0efb6c6a9eeb9548a
Naoya Ono <ono.naoyaa@gmail.com>
2018-01-04 17:08:23 +0800

    Added webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const BUILD_DIR = path.resolve(__dirname, 'app/webroot/');
+const APP_DIR = path.resolve(__dirname, 'app/src/');
+
+const config = {
+  entry: `${APP_DIR}/index.js`,
+  output: {
+    path: `${BUILD_DIR}/js/`,
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        include: APP_DIR,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
+
+module.exports = config;
