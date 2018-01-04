4fad94144a5b922a296e60e815d3dcd6b23af818
Naoya Ono <ono.naoyaa@gmail.com>
2018-01-04 18:27:27 +0800

    Created a component App.js

---
+++ app/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <p>Hello World</p>
+    );
+  }
+}
+
+export default App;
